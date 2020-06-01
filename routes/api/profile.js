149d01b237304aaae215014971365cddc8e73d2b
Maros Petrik <maros.petrik@inspire.cz>
2020-06-01 15:12:16 +0200

    Router for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const { User } = require("../../models/User");
+const { Profile } = require("../../models/Profile");
 
 // @route   api/profile/test
 // @desc    Test profile route
@@ -7,5 +12,25 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.json({ msg: "profile works" });
 });
+
+// @route   api/profile
+// @desc    gets current user profile route
+// @access  Private
+
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const errors = {};
+      const profile = await Profile.findOne({ user: req.user.id });
+      if (!profile) errors.noprofile = "There is not a profile for this user.";
+      return res.status(404).send(errors);
+      res.status(200).send(profile);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
 
 module.exports = router;
